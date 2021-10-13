13fff660dbafdcd2caeed4050e413989a0f7e611
qifeng <qifeng@botpy.com>
2021-10-13 10:44:22 +0800

    feat: 新建代理池app

---
+++ apps/proxy_pool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProxyPoolConfig(AppConfig):
+    name = "apps.proxy_pool"
---
+++ apps/proxy_pool/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from apps.proxy_pool import views
+
+urlpatterns = []
